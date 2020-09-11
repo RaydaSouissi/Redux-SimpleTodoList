c768898ce34deba991f0de26cda9e4ebcbc5541b
RaydaSouissi <rayda.souissi@esprit.tn>
2020-09-11 13:04:55 +0200

    simple redux todoList

---
+++ src/components/input.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {connect}from 'react-redux'
+class Input extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <input type="text" onChange={(e)=>{this.props.hundleChange(e.target)}}/>
+                <button onClick={()=>{this.props.add(this.props.input)}}> Add</button>
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps=(dispatch)=>{
+return {
+    hundleChange:(event) => {
+
+        dispatch({type:'SET_INPUT', value: event.value})
+    },
+    add:(input)=>{
+        let obj={}
+        let inputValue=input
+        let id=Math.floor(Math.random()*1000)
+        obj={inputValue,id}
+        dispatch({type:'ADD_INPUT', value:obj})
+
+    }
+}
+}
+const mapStateToProps=(state)=> {
+    return {
+        input: state.input
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Input);
